7f5956c04e05b966803b6cb03c425bafe744b70e
Dmitry V <wel4692@gmail.com>
2020-02-29 18:08:36 +0300

    add time live cookie

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,7 @@ const login = (req, res) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res
         .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
           httpOnly: true,
         })
         .send({ token });
