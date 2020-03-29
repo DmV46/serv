b282312e4d88dd8a7c7dda9bbd3aa04b467d52d5
Dmitry V <wel4692@gmail.com>
2020-03-29 16:30:56 +0300

    add domain in Readme.md and addCrashTest

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 app.use(errorLogger);
 
